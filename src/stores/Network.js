8f500510731cee7ac8b6bd3fd9612accef9c9c1f
takez0_o <takez0_o@protonmail.com>
2022-10-18 00:07:30 +0300

    ssl

--- src/stores/Network.js
+++ src/stores/Network.js
@@ -2,7 +2,7 @@ import { writable } from "svelte/store";
 import { ethers } from "ethers";
 export let nodeProvider = writable();
 export const connectNode = async () => {
-  const provider = new ethers.providers.JsonRpcProvider("http://decoded.wtf");
+  const provider = new ethers.providers.JsonRpcProvider("https://decoded.wtf");
   nodeProvider.set(provider);
 };
 connectNode();
